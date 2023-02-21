326916b5792716e0fa9208c6846e4cad6084b899
SaraS <sarasaco@ucm.es>
2023-02-21 14:50:15 +0100

    terminado lo de expense

--- Proyecto/src/main/resources/static/js/expense.js
+++ Proyecto/src/main/resources/static/js/expense.js
@@ -1,8 +1,16 @@
 function editar(){
-    document.getElementById("campaign").removeAttribute("readonly", false) ;
-    document.getElementById("campaign").value="editable";
+    document.getElementById("username").removeAttribute("readonly", false) ;
+    document.getElementById("username").value="Sari_";
+
+    document.getElementById("paid").removeAttribute("readonly", false) ;
+    document.getElementById("paid").value="yes_";
+
+    document.getElementById("amount").removeAttribute("readonly", false) ;
+    document.getElementById("amount").value="50_";
   }
   
   function bloquear(){
-     document.getElementById("campaign").setAttribute("readonly", true) ;
+     document.getElementById("username").setAttribute("readonly", true) ;
+     document.getElementById("paid").setAttribute("readonly", true) ;
+     document.getElementById("amount").setAttribute("readonly", true) ;
   }
