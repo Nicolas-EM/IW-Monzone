899abbbd3167abb60e7faf53ffa1b037c509f164
Nicolas-EM <nico.mooser@gmail.com>
2023-02-28 09:27:27 +0100

    Feat: Expense img preview

--- Proyecto/src/main/resources/static/js/expense.js
+++ Proyecto/src/main/resources/static/js/expense.js
@@ -1,18 +1,34 @@
-$(document).on("click", ".browse", function() {
-   var file = $(this).parents().find(".file");
-   console.log("cliiick");
-   file.trigger("click");
- });
+// $('input[type="file"]').change(function (e) {
+//   var fileName = e.target.files[0].name;
+//   $("#file").val(fileName);
 
- $('input[type="file"]').change(function(e) {
-   var fileName = e.target.files[0].name;
-   $("#file").val(fileName);
- 
-   var reader = new FileReader();
-   reader.onload = function(e) {
-     // get loaded data and render thumbnail.
-     document.getElementById("preview").src = e.target.result;
-   };
-   // read the image file as a data URL.
-   reader.readAsDataURL(this.files[0]);
- });
+//   var reader = new FileReader();
+//   reader.onload = function (e) {
+//     // get loaded data and render thumbnail.
+//     document.getElementById("preview").src = e.target.result;
+//   };
+//   // read the image file as a data URL.
+//   reader.readAsDataURL(this.files[0]);
+// });
+
+
+
+window.addEventListener("load", (event) => {
+  console.log("page is fully loaded");
+
+  document.getElementById('imgBtn').addEventListener('click', function(){
+    document.getElementById('imgFileInput').click();
+  });
+
+  document.getElementById('imgFileInput').addEventListener("change", function (e) {
+    console.log("change detected");
+
+    var reader = new FileReader();
+    reader.onload = function (e) {
+      // get loaded data and render thumbnail.
+      document.getElementById("imgBtn").src = e.target.result;
+    };
+    // read the image file as a data URL.
+    reader.readAsDataURL(this.files[0]);
+  });
+});
