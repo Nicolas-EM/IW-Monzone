4e2e7c3a8488a362c1ab2f027bc1e5cf84078e85
albgom21 <albgom21@ucm.es>
2023-04-08 16:00:27 +0200

    Cambio moneda visual

--- Proyecto/src/main/resources/static/js/ajax-userQueries.js
+++ Proyecto/src/main/resources/static/js/ajax-userQueries.js
@@ -1,16 +1,22 @@
 // envio de mensajes con AJAX
-document.getElementById("cur2").addEventListener('change', function(){
+document.getElementById("cur2").addEventListener('change', function () {
     const selector = document.getElementsByClassName('curr');
-    const values = document.getElementById("cur2").value;
+    const valueSelected = parseInt(document.getElementById("cur2").value);
     let currencyString = "";
-    console.log("onChangeCurrency(${currency}) called");
-    switch(selector){
-        case 0: 
-            currencyString= "€";
+    // Cambiar el tipo de moneda según la seleccionada
+    switch (valueSelected) {
+        case 0:
+            currencyString = "€";
+            break;
         case 1:
-            currencyString= "$";
+            currencyString = "$";
+            break;
         case 2:
-            currencyString= "£";
+            currencyString = "£";
+            break;
+        default:
+            currencyString = " ";
+            break;
     }
 
     for (let i = 0; i < selector.length; i++) {
