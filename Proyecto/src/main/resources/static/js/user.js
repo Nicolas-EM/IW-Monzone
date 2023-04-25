b685f76a64d8b43f649455975ae26b383eeaa4bd
albgom21 <albgom21@ucm.es>
2023-04-25 13:37:46 +0200

    Cambios en perfil (grupos) por AJAX

--- Proyecto/src/main/resources/static/js/user.js
+++ Proyecto/src/main/resources/static/js/user.js
@@ -53,3 +53,98 @@ document.getElementById("passwordForm").addEventListener('submit', (e) => {
             alert(JSON.parse(e.text).message);
         })
 });
+
+const groupsTable = document.getElementById("groupsTable");
+
+function getGroups() {
+    go(`${config.rootUrl}/user/getGroups`, "GET")
+        .then(groups => {
+            Array.from(groups).forEach(group => {
+                const elem = document.getElementById(`group-${group.id}`);
+                if (elem != null)
+                    groupsTable.removeChild(elem);
+                go(`${config.rootUrl}/group/${group.id}/getBalance`, "GET")
+                    .then(balance => {
+                        groupsTable.insertAdjacentHTML("afterbegin", renderGroup(group, balance));
+                    })
+            })
+        })
+        .catch(e => {
+            console.log("Error retrieving group", e);
+        })
+}
+
+// Render current groups
+getGroups();
+
+// Render group
+function renderGroup(group, balance) {   
+
+return `<div id="group-${group.id}" class="card text-white h-100 mx-auto">
+<label class="rounded-corners align-items-center w-100">
+  <div class="row">
+    <div class="groupName col-3">
+      ${group.name}
+    </div>
+    <div class="col-4">
+      Your budget: x ${group.currencyString}
+    </div>
+    <div class="col-5">
+      <span class="dot" style="${balance >= 0 ? 'background: green' : 'background: red'}"></span>
+      ${balance} ${group.currencyString}
+    </div>
+  </div>
+</label>
+</div>`
+}
+
+// Render INCOMING changes
+if (ws.receive) {
+    const oldFn = ws.receive; // guarda referencia a manejador anterior
+    ws.receive = (destination, obj) => {
+        oldFn(destination, obj); // llama al manejador anterior
+
+        if (obj.type == "GROUP") {
+            const group = obj.group;
+            const elem = document.getElementById(`group-${group.id}`);
+            switch (obj.action) {
+                case "GROUP_MEMBER_REMOVED": {
+                    go(`${config.rootUrl}/group/${group.id}/isMember`, "GET")
+                        .then(isMember => {
+                            elem.parentElement.removeChild(elem);
+                            if (isMember) {
+                                go(`${config.rootUrl}/group/${group.id}/getBalance`, "GET")
+                                    .then(balance => {
+                                        groupsTable.insertAdjacentHTML("afterbegin", renderGroup(group, balance));
+                                    })
+                            }
+                        })
+                } break;
+                case "GROUP_MODIFIED": {
+                    elem.parentElement.removeChild(elem);
+                    go(`${config.rootUrl}/group/${group.id}/getBalance`, "GET")
+                        .then(balance => {
+                            groupsTable.insertAdjacentHTML("afterbegin", renderGroup(group, balance));
+                        })
+                } break;
+                case "GROUP_DELETED": {
+                    elem.parentElement.removeChild(elem);
+                } break;
+            }
+        }
+
+        else if (obj.type === "EXPENSE") {
+            // get groupId from destination
+            const expGroupId = parseInt(destination.split("/")[3]);
+            const elem = document.getElementById(`group-${expGroupId}`);
+            go(`${config.rootUrl}/group/${expGroupId}/getGroupConfig`, "GET")
+                .then(group => {
+                    go(`${config.rootUrl}/group/${expGroupId}/getBalance`, "GET")
+                        .then(balance => {
+                            elem.parentElement.removeChild(elem);
+                            groupsTable.insertAdjacentHTML("afterbegin", renderGroup(group, balance));
+                        })
+                })
+        }
+    }
+}
