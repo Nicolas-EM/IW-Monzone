245635d037e2ec8c8b60bdb13ad45424ac4c2977
nico <nico.mooser@gmail.com>
2023-05-14 18:34:52 +0200

    Confirm destructive actions in group config

--- Proyecto/src/main/resources/static/js/group_config.js
+++ Proyecto/src/main/resources/static/js/group_config.js
@@ -28,10 +28,51 @@ if (btnConfirm != null) {  // NO para /group/new
     }
 }
 
+// Invite modal logic
 let modal = document.getElementById('inviteModal');
 if (modal != null) { // NO para /group/new
     modal.addEventListener('hidden.bs.modal', event => {
         document.getElementById("inviteUsername").value = "";
+    })
+}
+
+// Confirm modal logic (delete / leave)
+let confirmModal = document.getElementById('confirmModal')
+if (confirmModal) {
+    confirmModal.addEventListener('show.bs.modal', event => {
+        // Button that triggered the modal
+        const button = event.relatedTarget;
+
+        // Extract info from data-bs-* attributes
+        const btnType = button.getAttribute('data-bs-type');
+        const modalBody = document.getElementById('confirmModalBdy');
+        let removeId;
+        switch (btnType) {
+            case "leave":
+                modalBody.innerHTML = "Are you sure you want to leave this group?"
+                break;
+            case "delGroup":
+                modalBody.innerHTML = "Are you sure you want to delete this group?"
+                break;
+            case "delMember":
+                removeId = button.getAttribute('data-bs-removeId');
+                modalBody.innerHTML = "Are you sure you want to remove this person from this group?"
+                break;
+        }
+
+        document.getElementById('confirmBtn').onclick = () => {
+            switch (btnType) {
+                case "leave":
+                    leaveGroup();
+                    break;
+                case "delGroup":
+                    deleteGroup();
+                    break;
+                case "delMember":
+                    deleteMember(removeId);
+                    break;
+            }
+        }
     })
 }
 
@@ -42,18 +83,14 @@ function renderMember(member, currencyString) {
     if (isGroupAdmin === 'true' && member.idUser != userId) {
         memberHTML = `${memberHTML}
                     <div class="col btn-remove">
-                    <form method="post" action="/group/${groupId}/delMember">
-                        <input type="hidden" name="removeId" value="${member.idUser}">
-                        <input type="hidden" name="_csrf" value="${config.csrf.value}">
-                        <button type="submit" class="btn delMemberBtn">
-                            <svg id="trash" xmlns="http://www.w3.org/2000/svg" width="25" fill="white" class="bi bi-trash3" viewbox="0 0 16 16">
-                                <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z" />
-                            </svg>
-                        </button>
-                    </form>
+                    <button type="submit" class="btn" data-bs-toggle="modal" data-bs-target="#confirmModal" data-bs-type="delMember" data-bs-removeId="${member.idUser}">
+                        <svg id="trash" xmlns="http://www.w3.org/2000/svg" width="25" fill="white" class="bi bi-trash3" viewbox="0 0 16 16">
+                            <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z" />
+                        </svg>
+                    </button>
                 </div>`;
     }
-    else if (isGroupAdmin === 'true' && member.idUser == userId){
+    else if (isGroupAdmin === 'true' && member.idUser == userId) {
         memberHTML = `${memberHTML}
                 <div class="col btn-remove"></div>`;
     }
@@ -76,7 +113,6 @@ function renderMember(member, currencyString) {
 
 // Render member list
 function renderGroupMembers(group) {
-
     numMembers.innerHTML = group.numMembers;
 
     const membersTable = document.getElementById('membersTable');
@@ -87,29 +123,6 @@ function renderGroupMembers(group) {
             budget.innerHTML = member.budget;
         if (member.enabled)
             membersTable.insertAdjacentHTML("beforeend", renderMember(member, group.currencyString));
-    })
-
-    // Delete Member button function
-    Array.from(document.getElementsByClassName('delMemberBtn')).forEach(btn => {
-        btn.onclick = (e) => {
-            e.preventDefault();
-            go(btn.parentNode.action, 'POST', {
-                removeId: btn.parentNode.querySelector('input[name="removeId"]').value
-            })
-                .then(d => {
-                    console.log("Removed member");
-                    if (d.action == "redirect") {
-                        console.log("Redirecting to ", d.redirect);
-                        window.location.replace(d.redirect);
-                    } else {
-                        renderGroupData(group);
-                    }
-                })
-                .catch(e => {
-                    console.log("Failed to remove member", e);
-                    alert(JSON.parse(e.text).message);
-                })
-        }
     })
 }
 
@@ -135,12 +148,12 @@ if (groupId) {
     go(`${config.rootUrl}/group/${groupId}/getGroupConfig`, "GET")
         .then(group => {
             renderGroupData(group);
+            renderGroupMembers(group);
         }
         );
 }
 
 // Submit Button (SAVE)
-
 document.getElementById("groupForm").addEventListener('submit', (e) => {
     e.preventDefault();
     console.log('Saving group');
@@ -170,53 +183,67 @@ document.getElementById("groupForm").addEventListener('submit', (e) => {
         })
 });
 
-// Submit Button (DELETE GROUP)
+// DELETE GROUP
 // NOT for /group/new
-if (btnDelete != null) {
-    btnDelete.onclick = (e) => {
-        e.preventDefault();
-        console.log('Deleting group');
+function deleteGroup() {
+    console.log('Deleting group');
 
-        go(btnDelete.getAttribute('formaction'), 'POST')
-            .then(d => {
-                console.log("Group: success", d);
-                if (d.action === "redirect") {
-                    console.log("Redirecting to ", d.redirect);
-                    window.location.replace(d.redirect);
-                }
-            })
-            .catch(e => {
-                console.log("Error deleting group", e);
-                alert(JSON.parse(e.text).message);
-            })
-    };
+    go(btnDelete.getAttribute('formaction'), 'POST')
+        .then(d => {
+            console.log("Group: success", d);
+            if (d.action === "redirect") {
+                console.log("Redirecting to ", d.redirect);
+                window.location.replace(d.redirect);
+            }
+        })
+        .catch(e => {
+            console.log("Error deleting group", e);
+            alert(JSON.parse(e.text).message);
+        })
 }
 
-// Submit Button (LEAVE GROUP)
-if (btnLeave != null) {
-    btnLeave.onclick = (e) => {
-        e.preventDefault();
-        console.log('Leaving group');
-        const removeId = userId;
+// LEAVE GROUP
+function leaveGroup() {
+    console.log('Leaving group');
+    const removeId = userId;
 
-        go(btnLeave.getAttribute('formaction'), 'POST', {
-            removeId
+    go(btnLeave.getAttribute('formaction'), 'POST', {
+        removeId
+    })
+        .then(d => {
+            console.log("Group: success", d);
+            if (d.action === "redirect") {
+                console.log("Redirecting to ", d.redirect);
+                window.location.replace(d.redirect);
+            }
+            else {
+                console.log("Error leaving group");
+            }
         })
-            .then(d => {
-                console.log("Group: success", d);
-                if (d.action === "redirect") {
-                    console.log("Redirecting to ", d.redirect);
-                    window.location.replace(d.redirect);
-                }
-                else {
-                    console.log("Error leaving group");
-                }
-            })
-            .catch(e => {
-                console.log("Error leaving group", e);
-                alert(JSON.parse(e.text).message);
-            })
-    };
+        .catch(e => {
+            console.log("Error leaving group", e);
+            alert(JSON.parse(e.text).message);
+        })
+}
+
+// DELETE MEMBER
+function deleteMember(removeId) {
+    go(`/group/${groupId}/delMember`, 'POST', {
+        removeId
+    })
+        .then(d => {
+            console.log("Removed member");
+            if (d.action == "redirect") {
+                console.log("Redirecting to ", d.redirect);
+                window.location.replace(d.redirect);
+            } else {
+                //renderGroupData(group);
+            }
+        })
+        .catch(e => {
+            console.log("Failed to remove member", e);
+            alert(JSON.parse(e.text).message);
+        })
 }
 
 // Render INCOMING updates of group
@@ -228,14 +255,6 @@ if (ws.receive) {
         // If receiving a group and on that group
         if (obj.type == "GROUP" && obj.group.id == groupId) {
             console.log("Updating group view");
-            const redir = obj.action === "GROUP_DELETED";
-            if (obj.action === "GROUP_MEMBER_REMOVED") {
-                const member = obj.group.members.find(member => member.idUser == userId);
-                if (member != null && !member.enabled) {
-                    console.log("Redirecting to ", "/user/");
-                    window.location.replace("/user/");
-                }
-            }
             if (obj.action === "GROUP_DELETED") {
                 console.log("Redirecting to ", "/user/");
                 window.location.replace("/user/");
@@ -260,15 +279,15 @@ if (ws.receive) {
     }
 }
 
-document.getElementById('budget').addEventListener('input', function() {
+document.getElementById('budget').addEventListener('input', function () {
     const value = this.value.replace(/[^\d.]/g, ''); // remove any non-digit or non-decimal point characters
     const decimalIndex = value.indexOf('.');
     if (decimalIndex !== -1) {
-      const decimalPlaces = value.length - decimalIndex - 1;
-      if (decimalPlaces > 2) {
-        this.value = value.substring(0, decimalIndex + 3); // truncate to 2 decimal places
-        return;
-      }
+        const decimalPlaces = value.length - decimalIndex - 1;
+        if (decimalPlaces > 2) {
+            this.value = value.substring(0, decimalIndex + 3); // truncate to 2 decimal places
+            return;
+        }
     }
     this.value = value;
-  });
+});
